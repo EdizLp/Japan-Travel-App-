faffd04a39ee3136df7b092b998131c213dc1493
EdizLp <edizsuleyman404@gmail.com>
2026-02-13 01:03:25 +0000

    just did some small updates

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ from src.GoogleApi import request_information
 
 
 #this is effectively someone pasting a link, so requesting details 
-with open('./tests/test2.html', 'r', encoding='utf-8') as f: #Opening our test file using encoding for japanese characters
+with open('./tests/test2.html', 'r', encoding='utf-8') as f:                                                                                    #Opening our test file using encoding for japanese characters
     tabelogData = f.read()
-    soup = BeautifulSoup(tabelogData, 'html.parser') #Soup object for scraping with beautifulsoup
+    soup = BeautifulSoup(tabelogData, 'html.parser') 
 
-tabe_info = core_tabelog_information(soup)
-if tabe_info: #If we pulled the tabelog info 
-    tabe_url = tabe_info.get("URL") #Here's the url 
-    restaurant_info = check_master_records(tabe_url)#Check if we have that url
-    if restaurant_info:#if we have that url,if we do, we are given the json file as the return 
+tabe_info = core_tabelog_information(soup)                                                                                                      #Pulling Tabelog info
+if tabe_info: 
+    tabe_url = tabe_info.get("URL") 
+    restaurant_info = check_master_records(tabe_url)                                                                               
+    if restaurant_info:                                                                                                                         #if we have that url,if we do, we are given the json file as the return 
         print(restaurant_info) 
     else:
-        #if we dont have url
-        google_details_json = request_information(tabe_info)#our google json/dict
+        google_details_json = request_information(tabe_info)
         json_to_add = combine_two_dictionaries("Tabelog", "Google", tabe_info, google_details_json)
         check = add_to_records(json_to_add)
         if check:
