aa525ae0306f4445b24b2b0533c3549315dc6cf5
ediz333lp <edizsuleyman404@gmail.com>
2026-03-21 22:15:32 +0000

    Amended all the values in the GoogleFieldMap

--- src/google_formatter.py
+++ src/google_formatter.py
@@ -1,50 +1,67 @@
 from collections import namedtuple
 
 
-def process_google_data(data_source:dict, names_and_keys:list) -> dict: # Each one should return the data to export or None.
-    GoogleFieldMap = namedtuple("FieldMap", ["db_col_name", "json_key", "function"])
-    GOOGLE_MAPPING = [
-    GoogleFieldMap(db_col="google_id", json_key="id", function=extract_simple_data), #google id
-    GoogleFieldMap(db_col="google_id", json_key="internationalPhoneNumber", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="rating", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="viewport", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="googleMapsUri", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="websiteUri", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="formattedAddress", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="userRatingCount", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="delivery", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="goodForWatchingSports", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="liveMusic", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="outdoorSeating", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="reservable", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="servesBreakfast", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="servesCocktails", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="servesDinner", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="servesLunch", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="servesVegetarianFood", function=extract_simple_data),
-    GoogleFieldMap(db_col="google_id", json_key="takeout", function=extract_simple_data),
-    #Not simple
-    GoogleFieldMap(db_col="google_id", json_key="priceLevel", function=extract_simple_data),#price
-    GoogleFieldMap(db_col="google_id", json_key="location", function=extract_simple_data),#coords
-    GoogleFieldMap(db_col="google_id", json_key="displayName", function=extract_simple_data),#english name
-    GoogleFieldMap(db_col="google_id", json_key="primaryType", function=extract_simple_data),#Genre
-    GoogleFieldMap(db_col="google_id", json_key="types", function=extract_simple_data),#type
-    GoogleFieldMap(db_col="google_id", json_key="addressComponents", function=extract_simple_data),#prefecture and city
-    GoogleFieldMap(db_col="google_id", json_key="regularOpeningHours", function=extract_simple_data),#google hours
-    #Not simple and new 
-    GoogleFieldMap(db_col="google_id", json_key="takeout", function=extract_simple_data),#review summary
-    GoogleFieldMap(db_col="google_id", json_key="generativeSummary", function=extract_simple_data),#generative summary
-    GoogleFieldMap(db_col="google_id", json_key="paymentOptions", function=extract_simple_data),#payment options
-    GoogleFieldMap(db_col="google_id", json_key="priceRange", function=extract_simple_data),#price range 
-    GoogleFieldMap(db_col="google_id", json_key="photos", function=extract_simple_data),#photos
-    GoogleFieldMap(db_col="google_id", json_key="businessStatus", function=extract_simple_data)#business status 
-]
-    google_extract = {}
-    for pairs in names_and_keys:
-        data = 
-        google_extract[]
-    google_extract[]
+def process_google_data(data_source:dict) -> dict: 
+    if data_source.get("businessStatus") != "CLOSED_PERMANENTLY":
+        GoogleFieldMap = namedtuple("FieldMap", ["db_col_name", "json_key", "format_function"])
 
+        #GoogleFieldMap(db_col="google_id", json_key="photos", format_function=extract_simple_data) photos 
+
+        GOOGLE_MAPPING_MASTER = [
+        GoogleFieldMap(db_col="google_id", json_key="id", format_function=extract_simple_data), #google id - Nothing needs to change
+        GoogleFieldMap(db_col="phone_number", json_key="internationalPhoneNumber", format_function=extract_simple_data),#international phone number - Nothing needs to change
+        GoogleFieldMap(db_col="google_rating", json_key="rating", format_function=extract_simple_data),#google rating - Nothing needs to change
+        GoogleFieldMap(db_col="google_viewport", json_key="viewport", format_function=extract_simple_data),#viewport - Nothing needs to change
+        GoogleFieldMap(db_col="google_url", json_key="googleMapsUri", format_function=extract_simple_data),#google maps url - Nothing needs to change
+        GoogleFieldMap(db_col="website", json_key="websiteUri", format_function=extract_simple_data),#website url - Nothing needs to change
+        GoogleFieldMap(db_col="google_address", json_key="formattedAddress", format_function=extract_simple_data),#formatted address - Nothing needs to change
+        GoogleFieldMap(db_col="amount_google_ratings", json_key="userRatingCount", format_function=extract_simple_data),#user rating count  - Nothing needs to change
+        GoogleFieldMap(db_col="delivery_availability", json_key="delivery", format_function=extract_simple_data),#delivery - added
+        GoogleFieldMap(db_col="sports_watching", json_key="goodForWatchingSports", format_function=extract_simple_data),#sports
+        GoogleFieldMap(db_col="live_music", json_key="liveMusic", format_function=extract_simple_data),#live music?
+        GoogleFieldMap(db_col="outdoor_seating", json_key="outdoorSeating", format_function=extract_simple_data),#outdoor seating?
+        GoogleFieldMap(db_col="google_reservation", json_key="reservable", format_function=extract_simple_data),#reservable?
+        GoogleFieldMap(db_col="serves_breakfast", json_key="servesBreakfast", format_function=extract_simple_data),#breakfast
+        GoogleFieldMap(db_col="serves_cocktails", json_key="servesCocktails", format_function=extract_simple_data),#cocktails 
+        GoogleFieldMap(db_col="serves_dinner", json_key="servesDinner", format_function=extract_simple_data),#dinner
+        GoogleFieldMap(db_col="serves_lunch", json_key="servesLunch", format_function=extract_simple_data),#lunch
+        GoogleFieldMap(db_col="serves_vegetarian", json_key="servesVegetarianFood", format_function=extract_simple_data),#vegetarian? 
+        GoogleFieldMap(db_col="takeout_availability", json_key="takeout", format_function=extract_simple_data),#takeout
+        #Not simple
+        GoogleFieldMap(db_col="price_level", json_key="priceLevel", format_function=extract_simple_data),#price
+        GoogleFieldMap(db_col="google_coords", json_key="location", format_function=extract_simple_data),#coords
+        GoogleFieldMap(db_col="google_name", json_key="displayName", format_function=extract_simple_data),#english name
+        GoogleFieldMap(db_col="genre", json_key="primaryType", format_function=extract_simple_data),#Genre
+        GoogleFieldMap(db_col="type", json_key="types", format_function=extract_simple_data),#type
+        GoogleFieldMap(db_col="prefecture", json_key="addressComponents", format_function=extract_simple_data),#prefecture
+        GoogleFieldMap(db_col="city", json_key="addressComponents", format_function=extract_simple_data),#city
+        GoogleFieldMap(db_col="google_hours_mon", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours mon
+        GoogleFieldMap(db_col="google_hours_tue", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours tue
+        GoogleFieldMap(db_col="google_hours_wed", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours wed
+        GoogleFieldMap(db_col="google_hours_thu", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours thu
+        GoogleFieldMap(db_col="google_hours_fri", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours fri
+        GoogleFieldMap(db_col="google_hours_sat", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours sat
+        GoogleFieldMap(db_col="google_hours_sun", json_key="regularOpeningHours", format_function=extract_simple_data),#google hours sun
+        #Not simple and new 
+        GoogleFieldMap(db_col="review_summary", json_key="reviewSummary", format_function=extract_simple_data),#review summary
+        GoogleFieldMap(db_col="generative_summary", json_key="generativeSummary", format_function=extract_simple_data),#generative summary
+        GoogleFieldMap(db_col="accepts_card", json_key="paymentOptions", format_function=extract_simple_data),#payment options
+        GoogleFieldMap(db_col="cash_only", json_key="paymentOptions", format_function=extract_simple_data),#payment options
+        GoogleFieldMap(db_col="start_price", json_key="priceRange", format_function=extract_simple_data),#start price (as a string to include currency)
+        GoogleFieldMap(db_col="end_price", json_key="priceRange", format_function=extract_simple_data),#end price
+        
+        ]
+
+        google_extract = {}
+        for db_col, json_key, format_function in GoogleFieldMap:
+            raw_value = raw_google_json.get(json_key)
+        
+            if raw_value is not None:
+                # We just use whatever variable name we picked in the loop above
+                formatted_restaurant_data[db_col] = function(raw_value)
+
+            google_extract[]
+        google_extract[]
 
 
 def extract_simple_data(data_source) -> bool | None:
